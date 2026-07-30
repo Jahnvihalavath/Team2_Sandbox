ebcbb5fd145d2ee4a7d1264f1d442e854a1750f4
tramk1 <triramkad1@gmail.com>
2026-07-30 00:41:01 -0700

    ADV050, ADV051, ADV052

---
+++ backend/src/main/java/com/dbtraining/reconx/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package com.dbtraining.reconx.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
